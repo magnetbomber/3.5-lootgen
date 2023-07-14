b4ea080a44c17b9634e88659129e8d08d6f0090c
magnetbomber <69063484+magnetbomber@users.noreply.github.com>
2023-07-14 00:55:55 -0600

    Magic Item Basics Implemented

---
+++ MagicItem.cs
@@ -0,0 +1,73 @@
+﻿namespace LootGenerator_Three_Five;
+
+public interface MagicItemInternal : Treasure
+{
+    enum Tier
+    {
+        Mundane,
+        Minor,
+        Medium,
+        Major
+    }
+}
+
+public class MagicItem : Treasure
+{
+    MagicItemInternal item;
+    public int value()
+    {
+        return item.value();
+    }
+
+    public MagicItem()
+    {
+        GenerateNewMagicItem(MagicItemInternal.Tier.Mundane);
+    }
+
+    public MagicItem(int tier)
+    {
+        switch (tier)
+        {
+            case 1:
+                GenerateNewMagicItem(MagicItemInternal.Tier.Minor);
+                break;
+            case 2:
+                GenerateNewMagicItem(MagicItemInternal.Tier.Medium);
+                break;
+            case 3:
+                GenerateNewMagicItem(MagicItemInternal.Tier.Major);
+                break;
+            default:
+                GenerateNewMagicItem(MagicItemInternal.Tier.Mundane);
+                break;
+        }
+    }
+
+    public MagicItem(MagicItemInternal.Tier tier)
+    {
+        GenerateNewMagicItem(tier);
+    }
+
+    private void GenerateNewMagicItem(MagicItemInternal.Tier tier)
+    {
+        if (tier == MagicItemInternal.Tier.Mundane)
+        {
+            
+        }
+
+        if (tier == MagicItemInternal.Tier.Minor)
+        {
+            
+        }
+
+        if (tier == MagicItemInternal.Tier.Medium)
+        {
+            
+        }
+
+        if (tier == MagicItemInternal.Tier.Major)
+        {
+            
+        }
+    }
+}
---
+++ MagicItemTypes.cs
@@ -0,0 +1,82 @@
+﻿namespace LootGenerator_Three_Five;
+
+public class Armor : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Shield : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Weapon : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Potion : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Ring : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Rod : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Scroll : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Staff : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class Wand : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
+public class WondrousItem : MagicItemInternal
+{
+    private int v = 0;
+    public int value()
+    {
+        return v;
+    }
+}
